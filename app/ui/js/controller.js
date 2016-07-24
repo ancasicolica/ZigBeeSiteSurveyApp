1fe4ab36f6de0fcaeca5cedbc146171b15418b58
Christian Kuster <christian@kusti.ch>
2016-07-24 10:40:57 +0200

    Require instead of html script loading

--- app/ui/js/controller.js
+++ app/ui/js/controller.js
@@ -2,19 +2,30 @@
  * Created by kc on 22.07.16.
  */
 
+const $ = require('jquery');
+const _ = require('lodash');
 
 var surveyApp = angular.module('surveyApp', ['ngSanitize', 'ngCsv']);
-surveyApp.controller('surveyCtrl', ['$scope',  ($scope) => {
+surveyApp.controller('surveyCtrl', ['$scope', ($scope) => {
   var networkPool = core.getNetworkPool();
   $scope.settings = settings;
 
-  // Wait for changes in the wireless networks
-  core.on('networks', networks => {
-    $scope.zigBeeNetworks = _.sortBy( networkPool.getNetworks(), 'extendedPanId');
-    console.log('networks', $scope.zigBeeNetworks);
+  function updateNetworks() {
+    $scope.zigBeeNetworks = _.sortBy(networkPool.getNetworks(), 'extendedPanId');
+    console.log('networks found: ', $scope.zigBeeNetworks);
     $scope.$apply();
     // Create charts after $apply, otherwise the element is not available!
     createNetworkCharts($scope.zigBeeNetworks, $scope);
+  }
+
+  // Wait for changes in the wireless networks
+  core.on('networks', () => {
+    updateNetworks();
+  });
+
+  $(document).ready(() => {
+    console.log('document ready');
+    updateNetworks();
   });
 
 }]);
